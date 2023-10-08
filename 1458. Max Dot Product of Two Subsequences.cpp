f93cd2b2af1b480e2f38e5a95dd79e35afd01b14
dipesh617 <dipesh20181111@gmail.com>
2023-10-08 23:36:14 +0530

    added Max Dot Product of Two Subsequence

---
+++ 1458. Max Dot Product of Two Subsequences.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
+        int arr[505][505];
+        for(int i=0; i<=501; i++){
+            arr[0][i]=INT_MIN;
+
+        }
+        int maxi = INT_MIN;
+        for(int i=1; i<=nums1.size(); i++){
+            for(int j=1; j<=nums2.size(); j++){
+                if(i==1){
+                    arr[j][i]= max(arr[j-1][i],nums1[i-1]*nums2[j-1]);
+                    maxi = max(maxi,arr[j][i]);
+                }
+                else if(j==1){
+                    arr[j][i]=max(arr[j][i-1],nums1[i-1]*nums2[j-1]);
+                    maxi = max(maxi,arr[j][i]);
+                }
+                else{
+                    arr[j][i] = max({arr[j-1][i],arr[j][i-1],nums1[i-1]*nums2[j-1],arr[j-1][i-1],nums1[i-1]*nums2[j-1]+arr[j-1][i-1]});
+                    maxi = max(maxi,arr[j][i]);
+                }
+            }
+        }
+
+        return maxi;
+    }
+};
